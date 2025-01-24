69b31109dc40ad4ec3032dc8cf1dce802775c920
Teo Zheng Yang <teozhengyang15@gmail.com>
2025-01-24 09:46:16 +0800

    Edit navabr code

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Box, IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useMediaQuery } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import logo from '../assets/128px-TWICE.png';
 
 const Navbar = () => {
-  const isMobile = useMediaQuery('(max-width:600px)');
-
+  // Function to handle smooth scrolling
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
@@ -15,40 +12,73 @@ const Navbar = () => {
   };
 
   return (
-    <AppBar position="sticky" sx={{ backgroundColor: '#232323' }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: isMobile ? 'space-between' : 'flex-start', alignItems: 'center' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', marginRight: 2 }}>
-          <Button onClick={() => scrollToSection('header')} style={{ textDecoration: 'none' }}>
-            <img 
-              src={logo}
-              alt="Logo" 
-              style={{ width: '40px', height: '40px' }}
-            />
+    <>
+      <AppBar position="sticky" sx={{ backgroundColor: '#232323' }}>
+        <Toolbar sx={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+          {/* Logo on the left */}
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Button onClick={() => scrollToSection('header')} style={{ textDecoration: 'none' }}>
+              <img 
+                src={logo}
+                alt="Logo" 
+                style={{ width: '40px', height: '40px' }} // Adjust size of the logo as needed
+              />
+            </Button>
+          </Box>
+          
+          {/* Navbar sections */}
+          <Button
+            onClick={() => scrollToSection('about-me')}
+            sx={{
+              color: '#A0A0A0',
+              '&:hover': {
+                color: 'white',
+                backgroundColor: '#242424'
+              },
+            }}
+          >
+            About Me
           </Button>
-        </Box>
+          <Button
+            onClick={() => scrollToSection('experience')}
+            sx={{
+              color: '#A0A0A0',
+              '&:hover': {
+                color: 'white',
+                backgroundColor: '#232323',
+              },
+            }}
+          >
+            Experience
+          </Button>
+          <Button
+            onClick={() => scrollToSection('project')}
+            sx={{
+              color: '#A0A0A0',
+              '&:hover': {
+                color: 'white',
+                backgroundColor: '#232323'
+              },
+            }}
+          >
+            Projects
+          </Button>
+          <Button
+            onClick={() => scrollToSection('blog')}
+            sx={{
+              color: '#A0A0A0',
+              '&:hover': {
+                color: 'white',
+                backgroundColor: '#232323'
+              },
+            }}
+          >
+            Blog
+          </Button>
+        </Toolbar>
+      </AppBar>
 
-        {isMobile ? (
-          <IconButton color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-        ) : (
-          <>
-            <Button onClick={() => scrollToSection('about-me')} sx={{ color: '#A0A0A0', '&:hover': { color: 'white', backgroundColor: '#242424' } }}>
-              About Me
-            </Button>
-            <Button onClick={() => scrollToSection('experience')} sx={{ color: '#A0A0A0', '&:hover': { color: 'white', backgroundColor: '#232323' } }}>
-              Experience
-            </Button>
-            <Button onClick={() => scrollToSection('project')} sx={{ color: '#A0A0A0', '&:hover': { color: 'white', backgroundColor: '#232323' } }}>
-              Projects
-            </Button>
-            <Button onClick={() => scrollToSection('blog')} sx={{ color: '#A0A0A0', '&:hover': { color: 'white', backgroundColor: '#232323' } }}>
-              Blog
-            </Button>
-          </>
-        )}
-      </Toolbar>
-    </AppBar>
+    </>
   );
 };
 
