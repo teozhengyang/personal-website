2743ce2f7f6ec5deb2226cd8a2f70b3b01a135c9
Teo Zheng Yang <teozhengyang15@gmail.com>
2025-01-24 17:26:23 +0800

    Edit about me text

--- frontend/src/components/AboutMe.js
+++ frontend/src/components/AboutMe.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Box } from '@mui/material';
 import '../assets/AboutMe.css';
-import python_logo from '../assets/python.png';
-import java_logo from '../assets/java.png';
-import js_logo from '../assets/js.png';
 
 const AboutMe = () => {
   return (
@@ -11,50 +7,27 @@ const AboutMe = () => {
 
       <h1>About Me</h1>
 
-      <Box sx={{ display: 'flex', justifyContent: 'center', gap: '20px' }}>
-        <Box
-          component="img"
-          src={python_logo}
-          alt="Python Icon"
-          sx={{ width: 30, height: 30 }}
-        />
-        <Box
-          component="img"
-          src={java_logo}
-          alt="Java Icon"
-          sx={{ width: 30, height: 30 }}
-        />
-        <Box
-          component="img"
-          src={js_logo}
-          alt="JavaScript Icon"
-          sx={{ width: 30, height: 30 }}
-        />
-      </Box>
-
       <p class='about-me-text'> 
-        I'm a penultimate-year Computer Science student at NUS, specializing in database systems and software engineering. 
-        With a mix of school and self-learning, I've built a solid arsenal—tools like Figma, Docker, and AWS,
-        languages like Python, Java, and JavaScript, and frameworks like React, Flask, and Django. 
-        I see the big picture while sweating the small stuff, blending creativity with logic to craft software that’s smart, intuitive, and fun to use. 
-        Always hungry to level up and add more firepower to my toolkit!
+        I am a penultimate year Computer Science undergraduate at the National University of Singapore, specialising in database systems and software engineering.
+        My exploration of the field has led me to develop a keen interest in full-stack or front-end development.
+        I enjoy creating web applications that are both functional and visually appealing.
       </p>
 
       <br />
 
       <p class='about-me-text'> 
-        When I’m not coding, you’ll catch me glued to the screen, watching football, basketball, or the latest F1 race. 
-        I love the thrill of a good game, cheering for my favorite teams and athletes. 
-        When I'm not immersed in sports, I’m likely planning my next trip or enjoying a fun night out. 
-        After all, life’s too exciting to just stay behind the screen!
+        In my free time, I enjoy sports like football, basketball and F1. I also like to travel around the world and experience different cultures.
+        All these provides me a chance to escape from the digital world and recharge myself. I will be keen to explore photography in the future as well.
       </p>
 
-      <p class='about-me-text'>
-        Languages: Python, Java, JavaScript, TyoeScript, SQL, HTML, CSS, Solidity
+      <br />
+
+      <p id='about-me-tech'>
+        Languages: CSS, HTML, Java, JavaScript, Python, Solidity, SQL, TypeScript 
         <br />
-        Frameworks: React, Flask, Django
+        Frameworks: Django, Flask, React.js
         <br />
-        Others: Figma, Docker, AWS, Git, Jira, Selenium, Material UI
+        Others: Amazon Web Services, Docker, Figma, Git, Selenium
       </p>
 
     </div>
