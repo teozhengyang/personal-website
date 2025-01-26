b1bbcad328b9159a610ec348c967e59ec4f6957a
Teo Zheng Yang <teozhengyang15@gmail.com>
2025-01-26 21:09:50 +0800

    Add quote for each day of the week

--- frontend/src/components/ContactMe.js
+++ frontend/src/components/ContactMe.js
@@ -3,10 +3,27 @@ import { Box, Typography, Grid2, Button } from '@mui/material';
 import { LinkedIn, GitHub, Email } from '@mui/icons-material';
 
 const ContactMe = () => {
+
+    /* get current day */
+    const day = new Date();
+
+    const quotes = [
+      'A Sunday well-snoozed is a Sunday well-used.',
+      'Monday: because ‘Good Morning’ is an oxymoron.',
+      'Tuesday is just Monday’s ugly sister.',
+      'Wednesdays are for surviving, not thriving.',
+      'Thursday: Because every week needs a hero day.',
+      'Alexa, play ‘It’s Friday I’m in Love’.',
+      'I’m multi-slacking this Saturday.',
+    ]
+
   return (
     <Box sx={{ padding: { xs: 4, md: 10 }, backgroundColor: '#181818'}} id="contact">
       <Typography variant="h4" align="center" sx={{ marginBottom: { xs: 2, md: 4 }, fontWeight: 'bold' }}>
         Contact Me
+      </Typography>
+      <Typography variant="h6" align="center" sx={{ marginBottom: { xs: 2, md: 4 } }} fontFamily={'Consolas'}>
+        {quotes[day.getDay()]}
       </Typography>
       <Grid2 container spacing={2} justifyContent="center" alignItems="center">
         <Grid2 item>
