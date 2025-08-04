d5d8eef22da1d4e46d8d4d526fd3da260992ee05
Teo Zheng Yang <teozhengyang15@gmail.com>
2025-08-04 23:05:41 +0800

    edit footer to make the social links & random fact stack vertically on smaller screens

--- frontend/src/components/footer-section/footerSection.jsx
+++ frontend/src/components/footer-section/footerSection.jsx
@@ -3,9 +3,12 @@ import RandomFact from './randomFact';
 
 export default function FooterSection() {
   return (
-    <footer className="footer footer-center bg-base-200 text-lg p-10 rounded-xl mt-10 font-inter">
-      <SocialLinks />
-      <RandomFact />
+    <footer className="footer footer-center bg-base-200 text-base-content p-10 rounded-xl mt-10 font-inter">
+      <div className="flex flex-col lg:flex-row lg:justify-center lg:items-center gap-4 text-base">
+        <SocialLinks />
+        <div className="hidden lg:block w-px h-6 bg-base-content/20"></div>
+        <RandomFact />
+      </div>
     </footer>
   );
 }
