5e3a8083468a45843a459dc24bb26c3d4de14986
Teo Zheng Yang <teozhengyang15@gmail.com>
2026-01-05 18:28:04 +0800

    feat: update hero image and text

--- frontend/src/components/about-section/profileImage.jsx
+++ frontend/src/components/about-section/profileImage.jsx
@@ -1,4 +1,4 @@
-import mypicture from '../../assets/about-section/mypicture1.jpg';
+import mypicture from '../../assets/about-section/mypicture1.JPG';
 
 export default function ProfileImage() {
   return (
