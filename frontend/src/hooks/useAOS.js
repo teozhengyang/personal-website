6b67b779ca7a9be7a1968b8cdb75a1ae1d669017
Teo Zheng Yang <teozhengyang15@gmail.com>
2026-03-20 14:07:38 +0800

    edit useAOS hook

--- frontend/src/hooks/useAOS.js
+++ frontend/src/hooks/useAOS.js
@@ -2,8 +2,8 @@ import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-export default function useAOS(options = { duration: 1000, once: false }) {
+export default function useAOS() {
   useEffect(() => {
-    AOS.init(options);
-  }, [options]);
+    AOS.init({ duration: 1000, once: false});
+  }, []);
 }
